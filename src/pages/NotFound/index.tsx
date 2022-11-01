04d2b29f0df5e4fbb217375bef078d008860ea6a
rayoungpark <rayoung1229@gmail.com>
2022-11-01 18:29:06 +0900

    design: NotFound page 디자인 변경

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,10 +1,16 @@
+import { useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { NotFoundWrapper } from './styles';
 
 const NotFound = () => {
+  const navigate = useNavigate();
+  const onClickBackButton = useCallback(() => {
+    navigate('/');
+  }, []);
   return (
     <NotFoundWrapper>
       <h1>NOT FOUND</h1>
-      <button>뒤로가기</button>
+      <button onClick={onClickBackButton}>목록으로 가기</button>
     </NotFoundWrapper>
   );
 };
--- src/pages/NotFound/styles.tsx
+++ src/pages/NotFound/styles.tsx
@@ -5,12 +5,17 @@ export const NotFoundWrapper = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  height: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
 
   & h1 {
-    font-size: 32px;
-    font-weight: 600;
-    padding: 20px 0;
+    font-size: 50px;
+    font-weight: 700;
+    padding: 1em 0;
+    text-align: center;
   }
 
   & button {
@@ -21,5 +26,6 @@ export const NotFoundWrapper = styled.div`
     font-size: 20px;
     border-radius: 20px;
     margin-top: 20px;
+    cursor: pointer;
   }
 `;
